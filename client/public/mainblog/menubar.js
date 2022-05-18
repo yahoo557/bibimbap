fd3d71f19bad022d23105f6a49ad458469cc9764
jung0115 <jungim7490@gmail.com>
2022-05-18 16:39:05 +0900

    메뉴바 수정

--- client/public/mainblog/menubar.js
+++ client/public/mainblog/menubar.js
@@ -3,31 +3,47 @@ const menuIcon = document.getElementsByClassName('bi-list');
 const closeIcon = document.getElementsByClassName('bi-x');
 const menuBar = document.getElementsByClassName("menu-bar");
 
-// 메뉴바 열기
+const addIcon = document.getElementsByClassName("bi-box");
+const addView = document.getElementsByClassName("object-add");
+
+const editIcon = document.getElementsByClassName("bi-tools");
+
+const listIcon = document.getElementsByClassName("bi-file-text");
+
+// 메뉴바 보이기
 const showMenu = () => { 
     menuIcon[0].style.display = "none"; // 메뉴 아이콘 숨기기
     closeIcon[0].style.display = "block"; // 닫기 아이콘 보이기
     menuBar[0].style.left = "0vw"; // 메뉴 바 보이기
 }
-// 메뉴바 닫기
+// 메뉴바 숨기기
 const closeMenu = () => {
     menuIcon[0].style.display = "block"; // 메뉴 아이콘 보이기
     closeIcon[0].style.display = "none"; // 닫기 아이콘 숨기기
     menuBar[0].style.left = "-16vh"; // 메뉴 바 숨기기
+
+    addIcon[0].style.left = "0vh"; // 오브젝트 추가 버튼 비활성화
+    addView[0].style.display = "none"; // 오브젝트 추가 화면 숨기기
+
+    editIcon[0].style.left = "0vh"; // 편집 모드 버튼 비활성화
+
+    listIcon[0].style.left = "0vh"; // 게시물 리스트 버튼 비활성화
 }
 
 // 오브젝트 추가
 const objectAdd = () => {
-    const addIcon = document.getElementsByClassName("bi-box");
-    if(addIcon[0].style.left == "0vh")
+    if(addIcon[0].style.left == "0vh") {
         addIcon[0].style.left = "15vh"; // 오브젝트 추가 버튼 활성화
-    else
+        addView[0].style.display = "block"; // 오브젝트 추가 화면 보이기
+    }
+    else {
         addIcon[0].style.left = "0vh"; // 오브젝트 추가 버튼 비활성화
+        addView[0].style.display = "none"; // 오브젝트 추가 화면 숨기기
+    }
 }
 
 // 편집 모드
 const editMode = () => {
-    const editIcon = document.getElementsByClassName("bi-tools");
     if(editIcon[0].style.left == "0vh")
         editIcon[0].style.left = "15vh"; // 편집 모드 버튼 활성화
     else
@@ -36,7 +52,6 @@ const editMode = () => {
 
 // 게시물 리스트
 const postList = () => {
-    const listIcon = document.getElementsByClassName("bi-file-text");
     if(listIcon[0].style.left == "0vh")
         listIcon[0].style.left = "15vh"; // 게시물 리스트 버튼 활성화
     else
