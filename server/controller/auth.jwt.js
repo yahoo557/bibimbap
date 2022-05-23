d2b5f185711d2c27b42060630c7091e369a7e2d6
Seungbaek Lee <yahoo557@naver.com>
2022-05-24 06:51:39 +0900

    blog router 추가

--- server/controller/auth.jwt.js
+++ server/controller/auth.jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 
 
-const verifyToken = (res, req, next) => {
+verifyToken = (res, req, next) => {
   const parseCookie = str =>
     str.split(';').map(v => v.split('=')).reduce((acc, v) => {
       acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
@@ -16,15 +16,10 @@ const verifyToken = (res, req, next) => {
     });
   }
   jwt.verify(token, config.secret, (err, decoded) => {
-    if (err.name === 'TokenExpiredError') {
+    if (err) {
       return res.status(419).send({
-        message: "Expired token"
+        message: "Unauthenticate"
       });
-    }
-    if (err.name === 'JsonWebTokenError') {
-      return res.status(409).send({
-        message: "Unvalid token"
-      })
     }
     next();
   });
