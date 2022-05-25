16a251c3f3a9d3fe7c2377315171087a6951f0f3
sumin <gosumin0710@naver.com>
2022-05-25 22:05:09 +0900

    오브젝트 삭제 버튼 구현

--- client/public/mainblog/mainblog.js
+++ client/public/mainblog/mainblog.js
@@ -1088,3 +1088,23 @@ const saveBlob = (function() {
     };
 }());
 
+const removeObjectButton = document.getElementsByClassName('object-delete');
+removeObjectButton[0].addEventListener('click', () => {
+    if(objectEditButtons[0].classList.item(1)) {
+        const removeObjectImage = document.getElementsByClassName("remove-object-image");
+        
+        const selectedObjectKey = objectEditButtons[0].classList.item(1);
+        const selectedObjectTempleteId = objectAssign[selectedObjectKey]['template_id'];
+        console.log(selectedObjectTempleteId);
+        const selectedObjectTumbnailUrl = objectTemplate[selectedObjectTempleteId]['thumbnail_path'];
+        removeObjectImage[0].src = selectedObjectTumbnailUrl;
+
+    }
+
+
+    const removeObject = document.getElementsByClassName("remove-object");
+
+
+    removeObject[0].style.display = "block";
+    menuArea[0].style.display = "block";
+});
