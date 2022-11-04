8f2272b762977292c14db4c1029b8864954886eb
Seungbaek Lee <yahoo557@naver.com>
2022-11-04 11:11:31 +0900

    게시물 리스트 api 구현

--- server/router/postList.js
+++ server/router/postList.js
@@ -4,8 +4,25 @@ const client = require("../config/db.config"); // DB 연결
 const Query = require('pg').Query
 const path = require('path');
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'postList.html'));
+router.get("/:id", (req, res) => {
+    const blog_id = req.params.id;
+    const text = 'SELECT * FROM post Where user_id = (SELECT user_id From blog WHERE blog_id = $1);'
+    
+    client.query(text, [blog_id], (err, rows) => {
+        if(err){
+            return res.status(404).json({msg:err})
+        }
+        else {
+            //쿼리 결과가 있다면
+            if(rows.rows.length){
+                return res.status(200).json(rows.rows);
+            }
+            else{
+                return res.status(200).json({msg:"null"})
+            }
+            
+        }
+    })
 });
 
 
