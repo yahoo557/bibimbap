b36c67779ba95e91306875107946e37bd7de6a67
Seungbaek Lee <yahoo557@naver.com>
2022-11-07 22:15:35 +0900

    pull 땡기기전 commit

--- server/router/object/editObject.js
+++ server/router/object/editObject.js
@@ -3,12 +3,23 @@ const router = express.Router();
 const client = require('../../config/db.config.js');
 const dt = require('../../controller/decode.jwt.js');
 
-router.post('/:id', (req, res) => {
-    const objectId = req.params.id;
-    const queryEditObject = "UPDATE model_position, model_rotaion, model_scale FROM object WHERE object_id = $1 VALUE($2, $3, $4);"
-    client.query(queryEditObject, [objectId, req.body.position, req.body.rotation], (err, rows)=>{
-        
-    })
+router.post('/', (req, res) => {
+    if(!req.body.hasOwnProperty('objectID')) return res.status(500).send({msg: "잘못된 요청입니다."});
+    const targetObjectID = req.body.objectID;
+
+    // dt.decodeTokenPromise(req).then(decode => {
+
+    // })
+    const queryEditObject = "UPDATE object SET model_position = $1 , model_rotation = $2 , scale = $3 WHERE object_id = $4 returning *;"
+    client.query(queryEditObject, [req.body.position, req.body.rotation, req.body.scale, targetObjectID], (err, rows)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            return res.status(200).json({msg: "수정되었습니다."});
+        }
+    });
 })
 
 module.exports = router;
+
