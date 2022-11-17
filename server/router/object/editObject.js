bc966e82116de69d10cc9944f7da05f3afa00952
Seungbaek Lee <yahoo557@naver.com>
2022-11-17 11:38:07 +0900

    22.11.17 object edit 수정

--- server/router/object/editObject.js
+++ server/router/object/editObject.js
@@ -3,22 +3,27 @@ const router = express.Router();
 const client = require('../../config/db.config.js');
 const dt = require('../../controller/decode.jwt.js');
 
-router.post('/', (req, res) => {
-    if(!req.body.hasOwnProperty('objectID')) return res.status(500).send({msg: "잘못된 요청입니다."});
-    const targetObjectID = req.body.objectID;
+router.post('/:id', (req, res) => {
+   if (!req.body.hasOwnProperty('objectID')) return res.status(500).send({ msg: "잘못된 요청입니다." });
+   const targetObjectID = req.body.objectID;
 
-    // dt.decodeTokenPromise(req).then(decode => {
+   dt.decodeTokenPromise(req).then(decode => {
+      const objectId = req.params.id;
+      const getBlogDataSql = 'SELECT a.* FROM blog AS a INNER JOIN users AS b ON a.user_id = b.user_id AND b.username = $1';
+      client.query(getBlogDataSql, [decode.userData.username], (err, dataResult) => {
+         if (err) return res.status(500).send({ msg: "DB Error" });
+         if (dataResult.rows.length < 1) return res.status(500).send({ msg: "User Error" });
+         if (!dataResult.rows[0].object_list.includes(targetObjectID)) return res.status(500).send({ msg: "권한이 없습니다." });
+         const queryEditObject = "UPDATE model_position, model_rotaion, model_scale FROM object WHERE object_id = $1 VALUE($2, $3, $4);"
+         client.query(queryEditObject, [objectId, req.body.position, req.body.rotation], (err, rows) => {
+            if (err) return res.status(500).send({ msg: "DB Error - 2" });
+            return res.status(200).send({ msg: "수정되었습니다." });
+         });
+      });
+   }).catch(rea => {
+      return res.status(500).send({ msg: "인증 토큰 오류입니다." });
+   });
 
-    // })
-    const queryEditObject = "UPDATE object SET model_position = $1 , model_rotation = $2 , scale = $3 WHERE object_id = $4 returning *;"
-    client.query(queryEditObject, [req.body.position, req.body.rotation, req.body.scale, targetObjectID], (err, rows)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            return res.status(200).json({msg: "수정되었습니다."});
-        }
-    });
 })
 
 module.exports = router;
